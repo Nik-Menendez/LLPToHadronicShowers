2c031571e4fb3168dcc8b8d1fcca7094a0c21ad2
Sven Dildick <sven.dildick@cern.ch>
2021-02-25 10:18:22 -0600

    update samples

--- multicrab_RAW2DIGI_L1_ANA.py
+++ multicrab_RAW2DIGI_L1_ANA.py
@@ -1,4 +1,5 @@
 from CRABClient.UserUtilities import config
+from samples import *
 
 cconfig = config()
 cconfig.General.workArea = 'crab_projects'
@@ -6,42 +7,43 @@ cconfig.General.transferLogs = True
 cconfig.JobType.pluginName = 'Analysis'
 cconfig.Data.inputDBS = 'phys03'
 cconfig.Data.splitting = 'FileBased'
-cconfig.Data.unitsPerJob = 1
-cconfig.Data.outLFNDirBase = '/store/group/XXX/'
+cconfig.Data.unitsPerJob = 5
+cconfig.Data.outLFNDirBase = '/store/user/dildick/'
 cconfig.Site.storageSite = 'T3_US_FNALLPC'
 
-samples = {
-    '/HTo2LongLivedTo4b_MH-350_MFF-80_CTau-500mm_TuneCP5_14TeV_pythia8/Run3Winter20DRPremixMiniAOD-110X_mcRun3_2021_realistic_v6-v2/GEN-SIM-RAW',
-    '/HTo2LongLivedTo4b_MH-350_MFF-80_CTau-1000mm_TuneCP5_14TeV_pythia8/Run3Winter20DRPremixMiniAOD-110X_mcRun3_2021_realistic_v6-v2/GEN-SIM-RAW',
-    '/HTo2LongLivedTo4b_MH-350_MFF-80_CTau-10000mm_TuneCP5_14TeV_pythia8/Run3Winter20DRPremixMiniAOD-110X_mcRun3_2021_realistic_v6-v2/GEN-SIM-RAW',
-    '/HTo2LongLivedTo4b_MH-350_MFF-160_CTau-500mm_TuneCP5_14TeV_pythia8/Run3Winter20DRPremixMiniAOD-110X_mcRun3_2021_realistic_v6-v2/GEN-SIM-RAW',
-    '/HTo2LongLivedTo4b_MH-350_MFF-160_CTau-1000mm_TuneCP5_14TeV_pythia8/Run3Winter20DRPremixMiniAOD-110X_mcRun3_2021_realistic_v6-v2/GEN-SIM-RAW',
-    '/HTo2LongLivedTo4b_MH-350_MFF-160_CTau-10000mm_TuneCP5_14TeV_pythia8/Run3Winter20DRPremixMiniAOD-110X_mcRun3_2021_realistic_v6-v2/GEN-SIM-RAW',
-    '/HTo2LongLivedTo4b_MH-250_MFF-60_CTau-500mm_TuneCP5_14TeV_pythia8/Run3Winter20DRPremixMiniAOD-110X_mcRun3_2021_realistic_v6-v2/GEN-SIM-RAW',
-    '/HTo2LongLivedTo4b_MH-250_MFF-60_CTau-1000mm_TuneCP5_14TeV_pythia8/Run3Winter20DRPremixMiniAOD-110X_mcRun3_2021_realistic_v6-v2/GEN-SIM-RAW',
-    '/HTo2LongLivedTo4b_MH-250_MFF-60_CTau-10000mm_TuneCP5_14TeV_pythia8/Run3Winter20DRPremixMiniAOD-110X_mcRun3_2021_realistic_v6-v2/GEN-SIM-RAW',
-    '/HTo2LongLivedTo4b_MH-250_MFF-120_CTau-500mm_TuneCP5_14TeV_pythia8/Run3Winter20DRPremixMiniAOD-110X_mcRun3_2021_realistic_v6-v2/GEN-SIM-RAW',
-    '/HTo2LongLivedTo4b_MH-250_MFF-120_CTau-1000mm_TuneCP5_14TeV_pythia8/Run3Winter20DRPremixMiniAOD-110X_mcRun3_2021_realistic_v6-v2/GEN-SIM-RAW',
-    '/HTo2LongLivedTo4b_MH-250_MFF-120_CTau-10000mm_TuneCP5_14TeV_pythia8/Run3Winter20DRPremixMiniAOD-110X_mcRun3_2021_realistic_v6-v2/GEN-SIM-RAW',
-    '/HTo2LongLivedTo4b_MH-125_MFF-50_CTau-3000mm_TuneCP5_14TeV_pythia8/Run3Winter20DRPremixMiniAOD-110X_mcRun3_2021_realistic_v6-v2/GEN-SIM-RAW',
-    '/HTo2LongLivedTo4b_MH-125_MFF-50_CTau-30000mm_TuneCP5_14TeV_pythia8/Run3Winter20DRPremixMiniAOD-110X_mcRun3_2021_realistic_v6-v2/GEN-SIM-RAW',
-    '/HTo2LongLivedTo4b_MH-125_MFF-25_CTau-1500mm_TuneCP5_14TeV_pythia8/Run3Winter20DRPremixMiniAOD-110X_mcRun3_2021_realistic_v6-v2/GEN-SIM-RAW',
-    '/HTo2LongLivedTo4b_MH-125_MFF-25_CTau-15000mm_TuneCP5_14TeV_pythia8/Run3Winter20DRPremixMiniAOD-110X_mcRun3_2021_realistic_v6-v2/GEN-SIM-RAW',
-    '/HTo2LongLivedTo4b_MH-125_MFF-12_CTau-900mm_TuneCP5_14TeV_pythia8/Run3Winter20DRPremixMiniAOD-110X_mcRun3_2021_realistic_v6-v2/GEN-SIM-RAW',
-    '/HTo2LongLivedTo4b_MH-125_MFF-12_CTau-9000mm_TuneCP5_14TeV_pythia8/Run3Winter20DRPremixMiniAOD-110X_mcRun3_2021_realistic_v6-v2/GEN-SIM-RAW',
-    '/HTo2LongLivedTo4b_MH-1000_MFF-450_CTau-10000mm_TuneCP5_14TeV_pythia8/Run3Winter20DRPremixMiniAOD-110X_mcRun3_2021_realistic_v6-v2/GEN-SIM-RAW',
-    '/HTo2LongLivedTo4b_MH-1000_MFF-450_CTau-100000mm_TuneCP5_14TeV_pythia8/Run3Winter20DRPremixMiniAOD-110X_mcRun3_2021_realistic_v6-v2/GEN-SIM-RAW',
-    '/Nu_E10-pythia8-gun/Run3Winter20DRPremixMiniAOD-SNB_110X_mcRun3_2021_realistic_v6-v1/GEN-SIM-RAW',
-    '/Neutrino_Pt-2to20_gun/Run3Winter20DRPremixMiniAOD-SNB_110X_mcRun3_2021_realistic_v6-v1/GEN-SIM-RAW',
-}
 
-directory = '/uscms/home/dildick/nobackup/work/LLPStudiesWithSergoEtAL/CMSSW_11_1_0_pre6/src/'
+samples = [
+    HTo2LongLivedTo4b_MH_1000_MFF_450_CTau_100000mm,
+    HTo2LongLivedTo4b_MH_1000_MFF_450_CTau_10000mm,
+    HTo2LongLivedTo4b_MH_125_MFF_12_CTau_9000mm,
+    HTo2LongLivedTo4b_MH_125_MFF_12_CTau_900mm,
+    HTo2LongLivedTo4b_MH_125_MFF_25_CTau_15000mm,
+    HTo2LongLivedTo4b_MH_125_MFF_25_CTau_1500mm,
+    HTo2LongLivedTo4b_MH_125_MFF_50_CTau_30000mm,
+    HTo2LongLivedTo4b_MH_125_MFF_50_CTau_3000mm,
+    HTo2LongLivedTo4b_MH_250_MFF_120_CTau_10000mm,
+    HTo2LongLivedTo4b_MH_250_MFF_120_CTau_1000mm,
+    HTo2LongLivedTo4b_MH_250_MFF_120_CTau_500mm,
+    HTo2LongLivedTo4b_MH_250_MFF_60_CTau_10000mm,
+    HTo2LongLivedTo4b_MH_250_MFF_60_CTau_1000mm,
+    HTo2LongLivedTo4b_MH_250_MFF_60_CTau_500mm,
+    HTo2LongLivedTo4b_MH_350_MFF_160_CTau_10000mm,
+    HTo2LongLivedTo4b_MH_350_MFF_160_CTau_1000mm,
+    HTo2LongLivedTo4b_MH_350_MFF_160_CTau_5000mm,
+    HTo2LongLivedTo4b_MH_350_MFF_80_CTau_10000mm,
+    HTo2LongLivedTo4b_MH_350_MFF_80_CTau_1000mm,
+    HTo2LongLivedTo4b_MH_350_MFF_80_CTau_500mm,
+    HTo2LongLivedTo4q_MH_125_MFF_1_CTau_10000mm,
+    HTo2LongLivedTo4q_MH_125_MFF_1_CTau_1000mm,
+    HTo2LongLivedTo4q_MH_125_MFF_1_CTau_5000mm
+]
 
 if __name__ == '__main__':
     from CRABAPI.RawCommand import crabCommand
-    for s in samples:
-        cconfig.Data.inputDataset = s
-        cconfig.JobType.psetName = directory + 'L1Trigger/CSCTriggerPrimitives/test/CSCTPEmulator_cfg.py'
-        cconfig.General.requestName = cconfig.Data.inputDataset.split('/')[1] + '_RAW2DIGI_L1_ANA_20200515'
+    for sample in samples:
+        cconfig.Data.inputDataset = sample[1]
+        cconfig.General.requestName = sample[0] + '_ANA_20210225'
+        cconfig.Data.inputDBS = sample[2]
+        cconfig.JobType.psetName = sample[3]
         print cconfig
         #crabCommand('submit', config = cconfig)
--- setupDriverCommands.sh
+++ setupDriverCommands.sh
@@ -1,4 +1,4 @@
-cmsDriver.py LLPToHadronicShowers/Generator/python/ppTohToSSTo4b.py \
+ cmsDriver.py LLPToHadronicShowers/Generator/python/ppTohToSSTo4b.py \
 --fileout file:step1.root --mc \
 --eventcontent FEVTDEBUG,LHE --datatier GEN-SIM,LHE --conditions auto:phase1_2021_realistic \
 --beamspot Run3RoundOptics25ns13TeVLowSigmaZ --step LHE,GEN,SIM --geometry DB:Extended \
@@ -33,3 +33,26 @@ cmsDriver.py LLPToHadronicShowers/Generator/python/HTo2LongLivedTo4q_MH_125_MFF_
 --eventcontent FEVTDEBUG,LHE --datatier GEN-SIM,LHE --conditions auto:phase1_2021_realistic \
 --beamspot Run3RoundOptics25ns13TeVLowSigmaZ --step LHE,GEN,SIM --geometry DB:Extended \
 --era Run3 --python_filename HTo2LongLivedTo4q_MH_125_MFF_1_CTau_10000mm_TuneCP5_14TeV_pythia_GEN_SIM.py --no_exec   -n 10
+
+
+
+cmsDriver.py LLPToHadronicShowers/Generator/python/HTo2LongLivedTo4q_MH_125_MFF_1_CTau_10000mm_TuneCP5_14TeV_pythia8.py \
+--fileout file:step1.root --mc \
+--eventcontent FEVTDEBUG,LHE --datatier GEN-SIM,LHE,DIGI --conditions auto:phase1_2021_realistic \
+--beamspot Run3RoundOptics25ns13TeVLowSigmaZ --step LHE,GEN,SIM,DIGI --geometry DB:Extended \
+--era Run3 --python_filename HTo2LongLivedTo4q_MH_125_MFF_1_CTau_10000mm_TuneCP5_14TeV_pythia_GEN_SIM_DIGI.py --no_exec   -n 10
+
+
+
+
+
+
+cmsDriver.py Configuration/GenProduction/python/TSG-Run3Winter20GS-00040-fragment.py --python_filename TSG-Run3Winter20GS-00040_1_L1_cfg.py --eventcontent FEVTDEBUG --customise Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM-DIGI --fileout file:TSG-Run3Winter20GS-00040.root --conditions 110X_mcRun3_2021_realistic_v6 --beamspot Run3RoundOptics25ns13TeVLowSigmaZ --step GEN,SIM,DIGI:pdigi_valid,L1 --geometry DB:Extended --era Run3 --no_exec --mc -n 10 || exit $? ;
+
+## TSG numbers
+00040
+00064
+00065
+00066
+00067
+00068
