c03f71d7461e6c2826d7c4d5a8a8057ef239c8b2
Nik <mnik224x@gmail.com>
2021-06-19 15:39:36 -0500

    Combined efficiency and rate to a single function for simplicity

--- efficiency_rate.py
+++ efficiency_rate.py
@@ -3,23 +3,30 @@ import uproot4
 import awkward1 as ak
 import numpy as np
 import sys
+from collections import Counter
 
 in_file = str(sys.argv[1])
-debug = False
+find_rate = False
+if "Data" in in_file: find_rate = True
 
 #print("Getting Efficiency for file "+in_file)
 #print("")
-sample = in_file.split('_')
-HM = (sample[2])
-LLPM = (sample[4])
-CTau = (sample[6][0:-7])
+if not find_rate:
+	sample = in_file.split('_')
+	HM = (sample[2])
+	LLPM = (sample[4])
+	CTau = (sample[6][0:-7])
+else:
+	HM = "Zero"
+	LLPM = "Bias"
+	CTau = "Data"
 
-def get_eff(llp_accept,csc_accept,csc_accept_loose,csc_accept_tight,emtf_accept,emtf_accept_loose,gmt_accept):
+def get_eff(llp_accept,csc_accept,csc_accept_loose,csc_accept_tight,emtf_accept,emtf_accept_loose,gmt_accept,csc_sector,emtf_sector):
 
 	# Create a mask for events in acceptance
 	llp_pass = np.array(ak.sum(llp_accept, axis=-1)>0)
-	n_acc    = np.count_nonzero(llp_pass)
-	if n_acc==0: return [0,0,0,0]
+	#n_acc    = np.count_nonzero(llp_pass)
+	#if n_acc==0: return [0,0,0,0]
 
 	# Apppply mask to the csc, emtf, and gmt
 	csc_pass  = csc_accept[llp_pass]
@@ -28,87 +35,51 @@ def get_eff(llp_accept,csc_accept,csc_accept_loose,csc_accept_tight,emtf_accept,
 	csc_pass_loose = csc_accept_loose[llp_pass]
 	csc_pass_tight = csc_accept_tight[llp_pass]
 	emtf_pass_loose = emtf_accept_loose[llp_pass]	
+	n_acc = len(csc_pass)
 
 	# Calculate efficiency for each
-	csc_eff  = np.count_nonzero(ak.sum(csc_pass,axis=1))/n_acc*100
-	emtf_eff = np.count_nonzero(ak.sum(emtf_pass,axis=1))/n_acc*100
-	gmt_eff  = np.count_nonzero((gmt_pass))/n_acc*100
-	csc_eff_loose  = np.count_nonzero(ak.sum(csc_pass_loose,axis=1))/n_acc*100
-	csc_eff_tight  = np.count_nonzero(ak.sum(csc_pass_tight,axis=1))/n_acc*100
-	emtf_eff_loose = np.count_nonzero(ak.sum(emtf_pass_loose,axis=1))/n_acc*100
-	csc_eff_2loose = np.count_nonzero(np.array(ak.sum(csc_pass_loose,axis=1)>1))/n_acc*100
+	csc_eff  = np.count_nonzero(ak.sum(csc_pass,axis=1))/n_acc
+	emtf_eff = np.count_nonzero(ak.sum(emtf_pass,axis=1))/n_acc
+	gmt_eff  = np.count_nonzero((gmt_pass))/n_acc
+	csc_eff_loose  = np.count_nonzero(ak.sum(csc_pass_loose,axis=1))/n_acc
+	csc_eff_tight  = np.count_nonzero(ak.sum(csc_pass_tight,axis=1))/n_acc
+	emtf_eff_loose = np.count_nonzero(ak.sum(emtf_pass_loose,axis=1))/n_acc
+	#csc_eff_2loose = np.count_nonzero(np.array(ak.sum(csc_pass_loose,axis=1)>1))/n_acc
+	csc_avail = (csc_sector[llp_pass][ak.sum(csc_pass_loose,axis=1)>1])
+	csc_2loose = np.zeros(len(csc_avail))
+	i=0
+	for x in csc_avail:
+		a = np.array(x[np.array(csc_pass_loose[ak.sum(csc_pass_loose,axis=1)>1][i]>0)])
+		u, c = np.unique(a, return_counts=True)
+		if np.count_nonzero(c)>0: csc_2loose[i]=np.max(c)
+		else: csc_2loose[i]=0
+		i+=1
+	csc_eff_2loose = np.count_nonzero(csc_2loose>1)/n_acc
+	#if np.count_nonzero(csc_2loose>1) != np.count_nonzero(ak.sum(emtf_pass_loose,axis=1)): 
+	#	print("Mismatch")
+	#	#print(np.array((csc_2loose>1)))
+	#	#print(np.array((ak.sum(emtf_pass_loose[ak.sum(csc_pass_loose,axis=1)>1],axis=1)))>0)
+	#	index = np.where(np.equal(np.array((csc_2loose>1)),np.array((ak.sum(emtf_pass_loose[ak.sum(csc_pass_loose,axis=1)>1],axis=1)))>0)==False)[0]
+	#	print(index[0])
+	#	print("CSC Sectors with shower:")
+	#	print(csc_avail[index[0]])
+	#	print("CSC 1nom for the event:")
+	#	print(csc_pass[ak.sum(csc_pass_loose,axis=1)>1][index[0]])
+	#	print("CSC loose for the event:")
+	#	print(csc_pass_loose[ak.sum(csc_pass_loose,axis=1)>1][index[0]])
+	#	print("EMTF Sectors with shower:")
+	#	print(emtf_sector[llp_pass][ak.sum(csc_pass_loose,axis=1)>1][index[0]])
+	#	print("EMTF 2loose for the event:")
+	#	print(emtf_pass_loose[ak.sum(csc_pass_loose,axis=1)>1][index[0]])
+	#	print("EMTF 1nom for the event:")
+	#	print(emtf_pass[ak.sum(csc_pass_loose,axis=1)>1][index[0]])
 
 	emtf_one = np.array(ak.sum(emtf_pass,axis=1)>0)
 	emtf_two = np.array(ak.sum(emtf_pass_loose,axis=1)>0)
 	emtf_or = emtf_one | emtf_two
-	emtf_eff_or = np.count_nonzero(emtf_or)/n_acc*100
+	emtf_eff_or = np.count_nonzero(emtf_or)/n_acc
 
 	return [n_acc,csc_eff_loose,csc_eff,csc_eff_tight,csc_eff_2loose,emtf_eff,emtf_eff_loose,emtf_eff_or,gmt_eff]
-
-def get_rate(csc_accept,emtf_accept,gmt_accept):
-
-	# Calculate rate for reach
-	n_acc = len(csc_accept)
-	csc_rate  = np.count_nonzero(ak.num(csc_accept,axis=1))/n_acc*30*1000
-	emtf_rate = np.count_nonzero(ak.num(emtf_accept,axis=1))/n_acc*30*1000
-	gmt_rate  = np.count_nonzero(gmt_accept)/n_acc*30*1000
-
-	return [n_acc,csc_rate,emtf_rate,gmt_rate]
-
-def debug_eff(llp_accept,csc_accept,emtf_accept,gmt_accept,csc_endcap,csc_station,csc_ring,csc_chamber,emtf_endcap,emtf_station):
-	
-	# Create a mask for events in acceptance
-	llp_pass = np.array(ak.sum(llp_accept, axis=-1)>0)
-	
-	# Apppply mask to the csc, emtf, and gmt
-	csc_pass  = csc_accept[llp_pass]
-	emtf_pass = emtf_accept[llp_pass]
-	gmt_pass  = gmt_accept[llp_pass]
-
-	mismatch = ak.num(emtf_pass) > ak.flatten(gmt_pass)*5
-	print("Mismatch:")
-	print(mismatch)
-	print('')
-
-	csc_pass_endcap   = csc_endcap[llp_pass]
-	csc_pass_station = csc_station[llp_pass]
-	csc_pass_ring    = csc_ring[llp_pass]
-	csc_pass_chamber = csc_chamber[llp_pass]
-
-	print("CSC Masked Array:")
-	print(csc_pass[mismatch])
-	print("CSC endcap:")
-	print(csc_pass_endcap[mismatch])
-	print("CSC station:")
-	print(csc_pass_station[mismatch])
-	print("CSC ring:")
-	print(csc_pass_ring[mismatch])
-	print("CSC chamber:")
-	print(csc_pass_chamber[mismatch])
-	print("")
-	
-	emtf_pass_endcap = emtf_endcap[llp_pass]
-	emtf_pass_station = emtf_station[llp_pass]
-
-	print("EMTF Masked Array:")
-	print(emtf_pass[mismatch])
-	print("EMTF endcap:")
-	print(emtf_pass_endcap[mismatch])
-	print("EMTF sector:")
-	print(emtf_pass_station[mismatch])
-	print("")
-
-	print("GMT Masked Array:")
-	print(gmt_pass[mismatch])
-	print("")
-
-	# Calculate efficiency for each
-	n_acc    = np.count_nonzero(llp_pass)
-	csc_eff  = np.count_nonzero(ak.num(csc_pass,axis=1))/n_acc*100
-	emtf_eff = np.count_nonzero(ak.num(emtf_pass,axis=1))/n_acc*100
-	gmt_eff  = np.count_nonzero(gmt_pass)/n_acc*100
-
-	return [n_acc,csc_eff,emtf_eff,gmt_eff]
 
 def lct_check(csc_accept,csc_chamber,lct_chamber,alct_chamber,clct_chamber):
 
@@ -141,59 +112,22 @@ def lct_check(csc_accept,csc_chamber,lct_chamber,alct_chamber,clct_chamber):
 
 	return [total_cham,lct_eff,alct_eff,clct_eff]
 
-rate_arg = "eff"#str(sys.argv[1])
-find_rate = rate_arg == "rate"
 # Open file and tree
 tree = uproot4.open(in_file)["MuonNtuplizer"]["FlatTree"]
 
 # Save relevant branches into awkward arrays
 if not find_rate: llp_accept  = (tree["gen_llp_in_acceptance"].array())
+else: llp_accept = True
 csc_accept  = (tree["csc_shower_isNominalInTime"].array())
 emtf_accept = (tree["emtfshower_isOneNominalInTime"].array())
 gmt_accept  = (tree["l1mushower_isOneNominalInTime"].array())
 csc_accept_loose = (tree["csc_shower_isLooseInTime"].array())
 csc_accept_tight = (tree["csc_shower_isTightInTime"].array())
 emtf_accept_loose = (tree["emtfshower_isTwoLooseInTime"].array())
-if debug:
-	csc_endcap  = tree["csc_shower_region"].array()
-	csc_station = tree["csc_shower_station"].array()
-	csc_ring    = tree["csc_shower_ring"].array()
-	csc_chamber = tree["csc_shower_chamber"].array()
-	emtf_endcap = tree["emtfshower_region"].array()
-	emtf_station= tree["emtfshower_sector"].array()
+csc_sector = (tree["csc_shower_sector"].array())
+emtf_sector = tree["emtfshower_sector"].array()
 
-if debug: 
-	print("LLP_Acceptance array:")
-	print(llp_accept)
-	print("CSC_Acceptance array:")
-	print(csc_accept)
-	print("EMTF_Acceptance array:")
-	print(emtf_accept)
-	print("GMT_Acceptance array:")
-	print(gmt_accept)
-	print("")
-
-if not debug:
-	if find_rate: rates = get_rate(csc_accept,emtf_accept,gmt_accept)
-	else: effs = get_eff(llp_accept,csc_accept,csc_accept_loose,csc_accept_tight,emtf_accept,emtf_accept_loose,gmt_accept)
-else:
-	effs = debug_eff(llp_accept,csc_accept,emtf_accept,gmt_accept,csc_endcap,csc_station,csc_ring,csc_chamber,emtf_endcap,emtf_station)
-
-"""
-if find_rate:
-	# Print out results
-	print("Number of Events: %i"%(rates[0]))
-	print("CSC Trigger Rate: %.2f kHz"%(rates[1]))
-	print("EMTF Rate: %.2f kHz"%(rates[2]))
-	print("GMT Rate: %.2f kHz"%(rates[3]))
-else:
-	# Print out results
-	print("Number of Events with LLP in Acceptance: %i"%(effs[0]))
-	print("CSC Trigger Efficiency: %.2f%%" %(effs[1]))
-	print("EMTF Efficiency: %.2f%%" %(effs[2]))
-	print("GMT Efficiency: %.2f%%" %(effs[3]))
-#print("")
-"""
+effs = get_eff(llp_accept,csc_accept,csc_accept_loose,csc_accept_tight,emtf_accept,emtf_accept_loose,gmt_accept,csc_sector,emtf_sector)
 
 csc_chamber  = tree["csc_shower_chamber"].array()
 lct_chamber  = tree["csc_lct_chamber"].array()
@@ -202,11 +136,5 @@ alct_chamber = tree["csc_alct_chamber"].array()
 
 chams = lct_check(csc_accept,csc_chamber,lct_chamber,clct_chamber,alct_chamber)
 
-#print("Out of %i chambers with CSC showers:"%(chams[0]))
-#print("There are LCTs in %.2f%% of chambers"%(chams[1]))
-#print("There are ALCTs in %.2f%% of chambers"%(chams[2]))
-#print("There are CLCTs in %.2f%% of chambers"%(chams[3]))
-#
-#print("*************************************************************************")
-
-print("%s,%s,%s,%.2f%%,%.2f%%,%.2f%%,%.2f%%,%.2f%%,%.2f%%,%.2f%%,%.2f%%,%.2f%%,%.2f%%,%.2f%%"%(HM,LLPM,CTau,effs[1],effs[2],effs[3],effs[4],effs[5],effs[6],effs[7],effs[8],chams[1],chams[2],chams[3]))
+if not find_rate: print("%s,%s,%s,%.2f%%,%.2f%%,%.2f%%,%.2f%%,%.2f%%,%.2f%%,%.2f%%,%.2f%%,%.2f%%,%.2f%%,%.2f%%"%(HM,LLPM,CTau,effs[1]*100,effs[2]*100,effs[3]*100,effs[4]*100,effs[5]*100,effs[6]*100,effs[7]*100,effs[8]*100,chams[1],chams[2],chams[3]))
+else: print("%s,%s,%s,%.2f kHz,%.2f kHZ,%.2f kHZ,%.2f kHZ,%.2f kHZ,%.2f kHZ,%.2f kHZ,%.2f kHz,%.2f%%,%.2f%%,%.2f%%"%(HM,LLPM,CTau,effs[1]*30000,effs[2]*30000,effs[3]*30000,effs[4]*30000,effs[5]*30000,effs[6]*30000,effs[7]*30000,effs[8]*30000,chams[1],chams[2],chams[3]))
